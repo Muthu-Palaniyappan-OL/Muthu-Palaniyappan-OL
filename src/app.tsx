7e806c5978a09f81c7133620ea3eb6b52b7120d7
Muthu Palaniyappan <muthu892542@gmail.com>
2025-08-04 12:04:56 +0530

    Init

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import profileImage from "./assets/profile.png";
+import "./app.css";
+
+export function App() {
+  return (
+    <>
+      <p>Muthu Palaniyappan OL</p>
+      <img src={profileImage} height={400}></img>
+    </>
+  );
+}
